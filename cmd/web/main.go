c12fe2c5e731f0c52ce36eacbf2cd035673a401f
rickyjasso <rickyjassoa@gmail.com>
2023-08-23 22:41:28 -0600

    Created MySQL server and a db pool for the app

--- cmd/web/main.go
+++ cmd/web/main.go
@@ -1,10 +1,13 @@
 package main
 
 import (
+	"database/sql"
 	"flag"
 	"log"
 	"net/http"
 	"os"
+
+	_ "github.com/go-sql-driver/mysql" // (the blank identifier means that the package is imported for its side-effects only)
 )
 
 // create a new struct type to hold the application-wide dependencies for the web application.
@@ -19,6 +22,8 @@ func main() {
 	// The value of the addr flag is stored in the addr variable at runtime.
 	addr := flag.String("addr", ":4000", "HTTP network address")
 
+	dsn := flag.String("dsn", "web:9091@/snippetbox?parseTime=true", "MySQL data source name")
+
 	// Importantly, we use the flag.Parse() function to parse the command-line flag.
 	// This reads in the command-line flag value and assigns it to the addr
 	// variable. You need to call this *before* you use the addr variable
@@ -29,6 +34,13 @@ func main() {
 	// 3 parameters: destination to write the logs to, prefix message, flag to indicate what additional information to include in the log message
 	infoLog := log.New(os.Stdout, "INFO\t", log.Ldate|log.Ltime)                  // informational messages
 	errorLog := log.New(os.Stderr, "ERROR\t", log.Ldate|log.Ltime|log.Lshortfile) // error messages
+
+	db, err := openDB(*dsn)
+	if err != nil {
+		errorLog.Fatal(err)
+	}
+
+	defer db.Close()
 
 	// the app has to be a reference to the application struct type so that we can assign the loggers to the errorLog and infoLog fields.
 	// if it was a copy, the changes would not be reflected in the main function.
@@ -51,8 +63,20 @@ func main() {
 	infoLog.Printf("Starting server on %s", *addr) // informational message
 
 	// same thing. because srv is of type *http.Server, ListenAndServe() is a method on that type and can be used.
-	err := srv.ListenAndServe()
+	err = srv.ListenAndServe()
 	// err := http.ListenAndServe(*addr, mux)
 
 	errorLog.Fatal(err) // error message
+}
+
+func openDB(dsn string) (*sql.DB, error) {
+	db, err := sql.Open("mysql", dsn)
+	if err != nil {
+		return nil, err
+	}
+	if err = db.Ping(); err != nil {
+		// if there is an error, we return a nil *sql.DB connection pool and the error.
+		return nil, err
+	}
+	return db, nil
 }
